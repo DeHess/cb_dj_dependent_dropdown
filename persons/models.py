9141f102f25f060890e8ed8251df823a0c2dc8c6
Hess Nathan <nhess@baumer.com>
2023-12-20 14:10:59 +0100

    added additional countries field

--- persons/models.py
+++ persons/models.py
@@ -19,6 +19,7 @@ class City(models.Model):
 class Person(models.Model):
     name = models.CharField(max_length=124)
     country = models.ForeignKey(Country, on_delete=models.CASCADE, blank=True, null=True)
+    additional_countries = models.ManyToManyField(Country, related_name="eyo", blank=True, null=True)
     city = models.ManyToManyField(City, blank=True, null=True)
 
     def __str__(self):
